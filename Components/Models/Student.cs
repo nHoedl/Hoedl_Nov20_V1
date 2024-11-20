b0b028aae4b13c0d2e14549385d403af96f4a64a
nickh <nicholas.hoedl@ndsu.edu>
2024-11-20 13:45:04 -0600

    new Class

---
+++ Components/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Hoedl_Nov20.Components.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
